1faac499bddee95992dbf527084250a153eb50e7
JigarClarision <jigar@clarision.com>
2024-07-12 13:20:34 +0530

    convert to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 
     id("androidx.navigation.safeargs")
 
-//    id("dagger.hilt.android.plugin")
     id("com.google.dagger.hilt.android")
 
     id("com.google.gms.google-services")
@@ -83,7 +82,6 @@ dependencies {
     // dagger hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
-//    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     kapt("androidx.hilt:hilt-compiler:1.2.0")
 
     // firebase
