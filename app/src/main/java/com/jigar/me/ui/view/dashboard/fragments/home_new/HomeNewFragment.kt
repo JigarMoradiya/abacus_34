0dd041d49a061008776354f2f6489b5d7507e6c8
JigarMoradiya <jigar2596moradiya@gmail.com>
2025-12-07 11:03:28 +0530

    abacus base in jetpack compose canvas

--- app/src/main/java/com/jigar/me/ui/view/dashboard/fragments/home_new/HomeNewFragment.kt
+++ app/src/main/java/com/jigar/me/ui/view/dashboard/fragments/home_new/HomeNewFragment.kt
@@ -379,13 +379,12 @@ class HomeNewFragment : BaseFragment(), SelectAvatarProfileDialog.AvatarProfileD
                 mNavController?.navigate(R.id.action_homeFragment_to_myProfileFragment)
             }
             AppConstants.HomeClicks.Menu_Practice_Abacus -> {
-//                val action = HomeNewFragmentDirections.toCategoryFragment(data.id)
-//                mNavController?.navigate(action)
-                mNavController?.navigate(R.id.toAbacusFreeModeFragment)
+                val action = HomeNewFragmentDirections.toCategoryFragment(data.id)
+                mNavController?.navigate(action)
             }
             AppConstants.HomeClicks.Menu_Abacus_Free_Mode -> {
-                mNavController?.navigate(R.id.action_homeFragment_to_fullAbacusFragment)
-//                mNavController?.navigate(R.id.toAbacusFreeModeFragment)
+//                mNavController?.navigate(R.id.action_homeFragment_to_fullAbacusFragment)
+                mNavController?.navigate(R.id.toAbacusFreeModeFragment)
             }
             AppConstants.HomeClicks.Menu_Abacus_Exercise -> {
                 mNavController?.navigate(R.id.action_homeFragment_to_exerciseHomeFragment)
