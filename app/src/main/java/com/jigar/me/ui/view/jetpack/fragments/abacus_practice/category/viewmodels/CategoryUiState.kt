512a96e2c553fc67333480cfd33c92a66e9e2dd3
JigarMoradiya <jigar2596moradiya@gmail.com>
2025-12-20 19:15:07 +0530

    change abacus fetch data

--- app/src/main/java/com/jigar/me/ui/view/jetpack/fragments/abacus_practice/category/viewmodels/CategoryUiState.kt
+++ app/src/main/java/com/jigar/me/ui/view/jetpack/fragments/abacus_practice/category/viewmodels/CategoryUiState.kt
@@ -1,4 +1,4 @@
-package com.jigar.me.ui.view.jetpack.fragments.abacus_practice.category.model
+package com.jigar.me.ui.view.jetpack.fragments.abacus_practice.category.viewmodels
 
 import com.jigar.me.data.model.dbtable.abacus_all_data.Category
 import com.jigar.me.data.model.dbtable.abacus_all_data.DisplayPages
--- app/src/main/java/com/jigar/me/ui/view/jetpack/fragments/abacus_practice/category/viewmodels/CategoryViewModel.kt
+++ app/src/main/java/com/jigar/me/ui/view/jetpack/fragments/abacus_practice/category/viewmodels/CategoryViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.jigar.me.ui.view.jetpack.core.StatefulViewModel
 import com.jigar.me.ui.view.jetpack.fragments.abacus_practice.category.CategoryRepository
-import com.jigar.me.ui.view.jetpack.fragments.abacus_practice.category.model.CategoryUiState
+import com.jigar.me.ui.view.jetpack.fragments.abacus_practice.category.viewmodels.CategoryUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
